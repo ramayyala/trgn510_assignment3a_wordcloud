60b3f816196a60e46ba3f406349f5d6857febf3a
Ram <ramayyala@gmail.com>
2021-09-16 18:41:14 -0700

    finished

--- generate_wordcloud_from_file.sh
+++ generate_wordcloud_from_file.sh
@@ -2,8 +2,8 @@
 counter=1
 while read line; do
     wget -O /home/rayyala/TRGN510/assignments/trgn_wordcloud/current_pages/file${counter}.html ${line}
-    html2text.py /home/rayyala/TRGN510/assignments/trgn_wordcloud/current_pages/file${counter}.html >> my_current.txt
-    wordcloud_cli --text my_current.txt --imagefile ~/public_html/wordcloud.png
+    ~/bin/html2text.py /home/rayyala/TRGN510/assignments/trgn_wordcloud/current_pages/file${counter}.html >> /home/rayyala/TRGN510/assignments/trgn_wordcloud/my_current.txt
+    ~/.local/bin/wordcloud_cli --text /home/rayyala/TRGN510/assignments/trgn_wordcloud/my_current.txt --imagefile /home/rayyala/public_html/wordcloud.png
     counter=$[${counter} +1]
-done < my_webpages.txt 
+done < /home/rayyala/TRGN510/assignments/trgn_wordcloud/my_webpages.txt 
 echo "Successfully run for $USER";
